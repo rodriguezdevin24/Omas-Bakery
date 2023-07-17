59f8a8acaef37783bd87ee484652677163cda3bc
rodriguezdevin24 <131619471+rodriguezdevin24@users.noreply.github.com>
2023-07-17 12:12:09 -0400

    App Structure

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Carousel from '../Carousel/Carousel';
+import Footer from '../Footer/Footer';
+import NavBar from '../NavBar/NavBar';
+import ImageScroll from '../ImageScroll/ImageScroll';
+
+const MainPage = () => {
+    return (
+      <div>
+        <h1>Omas bakery</h1>
+      </div>
+    );
+  }
+  
+  export default MainPage;
+  
