ab7c3e7767a82765dbf5b9218c22091bdfab7696
Jamari02 <castrojamari@gmail.com>
2023-07-17 17:48:57 -0400

    last step

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import MainPage from './components/MainPage/MainPage';
 import './App.css';
-
+import HorizonalScroll from './components/HorizonalScroll/HorizonalScroll';
+import TestHorizontal from './components/HorizonalScroll/TestHorizontal';
 
 function App() {
   return (
     <div>
       <MainPage />
+      <HorizonalScroll/>
     </div>
   );
 }
---
+++ src/components/HorizonalScroll/HorizonalScroll.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import '../HorizonalScroll/horizontalscroll.css';
+
+const HorizontalScroll = () => {
+    return (
+      <div className="box">
+        <div className="horizontal-scroll-wrapper">
+          <div className="horizontal-scroll">
+            <div className="overlap">
+              <img className="rectangle" alt="Rectangle" src="../../images/Horizontal scroll/pic1.png" />
+              <div className="text-wrapper">Krapfen</div>
+            </div>
+            <div className="overlap">
+              <img className="img" alt="Rectangle" src="../../images/Horizontal scroll/pic2.png" />
+            <div className="apfelstusel">Apfelstrudel</div>
+            </div>
+            <div className="overlap">
+              <img className="rectangle-2" alt="Rectangle" src="../../images/Horizontal scroll/pic3.png" />
+            <div className="text-wrapper-2">Sachertorte</div>
+            </div>
+            <div className="overlap">
+              <img className="rectangle-3" alt="Rectangle" src="../../images/Horizontal scroll/pic4.png" />
+            <div className="text-wrapper-5">Schaumrolle</div>
+            </div>
+            <div className="overlap">
+              <img className="rectangle-4" alt="Rectangle" src="../../images/Horizontal scroll/pic5.png" />
+            <div className="text-wrapper-3">Lebkuchen</div>
+            </div>
+            <div className="overlap">
+              <img className="rectangle-5" alt="Rectangle" src="../../images/Horizontal scroll/pic6.png" />
+            <div className="text-wrapper-6">Streuselkuchen</div>
+            </div>
+            <div className="overlap">
+              <img className="rectangle-6" alt="Rectangle" src="../../images/Horizontal scroll/pic7.png" />
+            <div className="text-wrapper-4">Franzbrötchen</div>
+            </div>
+            <div className="overlap">
+              <img className="img" alt="Rectangle" src="../../images/Horizontal scroll/pic8.png" />
+            <div className="mandel-ho-rnchen">
+              Mandel-
+              <br />
+              hörnchen
+            </div>
+            </div>
+            {/* <div className="label"> */}
+            <div className="apfelstusel-wrapper">
+           </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+export default HorizonalScroll;
