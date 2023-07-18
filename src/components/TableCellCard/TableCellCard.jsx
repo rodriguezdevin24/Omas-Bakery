f2d10eaad9d26ee06deb43fa106b5c325cedddb1
Alison <alison.bayona@gmail.com>
2023-07-18 10:10:12 -0400

    hot

---
+++ src/components/TableCellCard/TableCellCard.jsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import img10 from '../../SEI collab Assets/Cards/StackedCard/stackedcard1.png';
+// import img20 from '';
+// import img30 from '';
+
+
+// function TableCellCard() {
+//   return (
+//     <div>
+//        <h2>Opa's Favorites</h2>
+//         <div className="shape-container">
+//         <img src={img10} alt="rectangle" className="img1" />
+//         <img src={img20} alt="rectangle" className="img2" />
+//         <img src={img30} alt="rectangle" className="img3" />
+//       </div>
+
+//     </div>
+//   )
+// }
+
+// export default TableCellCard
