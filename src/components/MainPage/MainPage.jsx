c1e8438f46d22f505569f5a064048be5afc4b446
rodriguezdevin24 <131619471+rodriguezdevin24@users.noreply.github.com>
2023-07-18 04:45:30 -0400

    completed carousel

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-// import Carousel from '../Carousel/Carousel';
+import Carousel from '../Carousel/Carousel';
+import image1 from '../../SEIcollabAssets/Carousel/Carousel1.png';
+import image2 from '../../SEIcollabAssets/Carousel/Carousel2.png';
+import image3 from '../../SEIcollabAssets/Carousel/Carousel3.png';
+import image4 from '../../SEIcollabAssets/Carousel/Carousel4.png';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import '../Carousel/carousel.css';
 // import Footer from '../Footer/Footer';
 // import NavBar from '../NavBar/NavBar';
 // import HorizonalScroll from '../HorizonalScroll/HorizonalScroll';
 // import FullCard from '../FullCard/FullCard';
 // import SplitCard from '../SplitCard/SplitCard';
 // import TableCellCard from '../TableCellCard/TableCellCard';
-import Navbar from '../NavBar/NavBar';
+//import Navbar from '../NavBar/NavBar';
 import '../NavBar/navbar.css';
 
 
 const MainPage = () => {
+    const images = [image1, image2, image3, image4];
   return (
     <div>
-      <Navbar /> {/* Add the Navbar component */}
+      <Carousel images={images}  showThumbs={false} /> 
     </div>
   );
 };
