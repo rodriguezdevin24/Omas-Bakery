865fb6fa9408029d43e7f02534c98b577cb2a69b
genpai99 <darioussanchez@gmail.com>
2023-07-18 11:43:01 -0400

    Fixed the CSS for the split card

--- src/components/SplitCard/SplitCard.jsx
+++ src/components/SplitCard/SplitCard.jsx
@@ -1,41 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { UpfiLittleButton } from "./UpfiLittleButton";
 import "./splitcard.css";
+import { useTheme } from '../theme';
 
 const SplitCard = ({ className }) => {
-  return (
-    <div className={`UPFI-split-card-container ${className}`}>
-      <div className={`UPFI-light-split ${className}`}>
-        <div className="div">
-          <div className="UPFI-split-card-dark">
-            <div className="div">
-              <img className="group" alt="Group" src="/images/Cards/splitcard.png" />
+    const [theme, setTheme] = useTheme();
+
+    useEffect(() => {
+      document.body.className = theme;
+    }, [theme]);
+
+    const containerClassName = `UPFI-split-card-container ${theme} ${className || ""}`;
+
+    return (
+        <div className={containerClassName}>
+            <div className={`UPFI-light-split  ${className || ""}`}>
+                <div className="div">
+                    <div className="UPFI-split-card-dark">
+                        <div className="div">
+                            <img className="group" alt="Group" src="/images/Cards/splitcard.png" />
+                        </div>
+                    </div>
+                    <div className="schaumrolle">Schaumrolle</div>
+                    <p className="a-delicate-pastry">
+                        A delicate pastry filled with vanilla cream and encased in a crispy, golden puff pastry shell - a delightful
+                        and authentic Viennese treat
+                    </p>
+                    <UpfiLittleButton className="UPFI-little-button-instance" />
+                </div>
             </div>
-          </div>
-          <div className="schaumrolle">Schaumrolle</div>
-          <p className="a-delicate-pastry">
-            A delicate pastry filled with vanilla cream and encased in a crispy, golden puff pastry shell - a delightful
-            and authentic Viennese treat
-          </p>
-          <UpfiLittleButton className="UPFI-little-button-instance" />
-        </div>
-      </div>
-      <div className={`UPFI-light-split ${className}`}>
-        <div className="div">
-          <div className="UPFI-split-card-dark">
-            <div className="div">
-              <img className="group" alt="Group" src="/images/Cards/splitcard.png" />
+            <div className={`UPFI-light-split  ${className || ""}`}>
+                <div className="div">
+                    <div className="UPFI-split-card-dark">
+                        <div className="div">
+                            <img className="group" alt="Group" src="/images/Cards/splitcard.png" />
+                        </div>
+                    </div>
+                    <div className="schaumrolle">Krapfen</div>
+                    <p className="a-delicate-pastry">
+                        Delectable Austrian doughnuts filled with luscious creams and fruit compotes, handcrafted to perfection
+                    </p>
+                    <UpfiLittleButton className="UPFI-little-button-instance" />
+                </div>
             </div>
-          </div>
-          <div className="schaumrolle">Krapfen</div>
-          <p className="a-delicate-pastry">
-          Delectable Austrian doughnuts filled with luscious creams and fruit compotes, handcrafted to perfection
-          </p>
-          <UpfiLittleButton className="UPFI-little-button-instance" />
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SplitCard;
